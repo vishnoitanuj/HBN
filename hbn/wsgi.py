3a3f57fbac6936de4a7a9bdfdc20e76e46338e06
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2017-06-09 17:45:23 +0530

    Add Requirements

--- hbn/wsgi.py
+++ hbn/wsgi.py
@@ -16,10 +16,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hbn.settings")
 application = get_wsgi_application()
 
 try:
-    from whitenoise.django import DjangoWhiteNoise
+    from dj_static import Cling
 
-    application = get_wsgi_application()
-    application = DjangoWhiteNoise(application)
+    application = Cling(get_wsgi_application())
 
 except:
     pass
