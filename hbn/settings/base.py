2764c75729ff3b97d065a5f261655c19dfbc5f3a
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2017-06-09 20:04:56 +0530

    New dj-database-url

--- hbn/settings/base.py
+++ hbn/settings/base.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-STATIC_ROOT = os.path.normpath(os.path.normpath(os.path.join(BASE_DIR, 'staticfiles')))
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
