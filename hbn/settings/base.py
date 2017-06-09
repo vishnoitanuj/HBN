6910e63d7ce1e4f15a505344a013007618b9374c
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2017-06-09 21:19:19 +0530

    Update Settings1

--- hbn/settings/base.py
+++ hbn/settings/base.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-STATIC_ROOT = os.path.normpath(os.path.os.path.join(BASE_DIR, 'staticfiles'))
+STATIC_ROOT = os.path.normpath(os.path.normpath(os.path.os.path.join(BASE_DIR, 'staticfiles')))
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
